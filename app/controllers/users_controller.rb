7b964afebfdfd1dbdae1964bb4e103dd10a8bc58
Maarten Griffioen <M_Griffioen@hotmail.com>
2018-10-12 15:12:48 +0200

    Add account activation but skipped email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user,  only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,    only: [:edit, :update]
+  before_action :logged_in_user,  only: %i[index edit update destroy]
+  before_action :correct_user,    only: %i[edit update]
   before_action :admin_user,      only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.active.paginate(page: params[:page])
   end
 
   def new
@@ -14,9 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      flash[:info] = 'Please check your email to activate your account'
+      redirect_to root_url
     else
       render 'new'
     end
@@ -29,6 +28,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url && return unless @user.activated?
   end
 
   def update
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Please log in."
+      flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
   end
